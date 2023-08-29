ed7bb1d69567145ce574978f159478fdc71a1573
IagoBonfim <iagoBonfim@users.noreply.github.com>
2023-08-29 16:12:05 -0300

    atualização
    inclusão de novas imagens e atualização do script.

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const saldo = document.querySelector('#saldo')
-let porquinho = document.querySelectorAll('.imagemPorquinho')
-const imagens = ['Adicionar_porquinho.jpg', 'Quebrado.png']
+let porquinho = document.querySelector('.imagemPorquinho')
+const imagens = ['Adicionar_porquinho.jpg', 'Quebrado.png','magro.jpg']
 let armazenado = []
 
-function Adicionar(){
-    armazenado.push(Number(adicionar.value)) 
-    porquinho.innerHTML = `<img src="/imagens/${imagens[0]}">`
-    console.log('aqui', porquinho);
+function Adicionar() {
+    if (Number(adicionar.value) < 1) {
+        alert('Adicione um valor maior!')
+    } else {
+        armazenado.push(Number(adicionar.value))
+        saldo.innerHTML = "Uhul, o dinheiro foi adicionado!"
+        escreverInnerHTML(imagens[0])
+    }
 }
-function Quebrar(){
-    let total = armazenado.reduce((soma, i) => {return soma + i }) 
-    total > 999 ?
-        saldo.innerHTML = `${(total).toLocaleString('pt-br',{style:'currency', currency: 'BRL'})}`
-            : alert('Seu porquinho não é gordo o suficiente!')
+function Quebrar() {
+    let total = armazenado.reduce((soma, i) => { return soma + i })
+    if (total < 1000) {
+        saldo.innerHTML = `Seu porquinho não é gordo o suficiente!`
+        escreverInnerHTML(imagens[2])
+    } else {
+        saldo.innerHTML = `${(total).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`
+        escreverInnerHTML(imagens[1])
+    }
+}
+function escreverInnerHTML(texto){
+     return porquinho.innerHTML = `<img width="300" src="./imagens/${texto}">`
 }
