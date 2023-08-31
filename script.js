7b398d9e67a15252aa5b3b8f3b6034815b180fde
IagoBonfim <iagoBonfim@users.noreply.github.com>
2023-08-31 17:35:40 -0300

    inclusão de eventos dos botões
    Atualização da chamada de evento, migração do HTML para o JAVASCRIPT

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 const saldo = document.querySelector('#saldo')
+
 let porquinho = document.querySelector('.imagemPorquinho')
 const imagens = ['Adicionar_porquinho.jpg', 'Quebrado.png','magro.jpg']
 let armazenado = []
 
-function Adicionar() {
-    if (Number(adicionar.value) < 1) {
-        alert('Adicione um valor maior!')
-    } else {
-        armazenado.push(Number(adicionar.value))
-        saldo.innerHTML = "Uhul, o dinheiro foi adicionado!"
-        alert(`R$ ${Number(adicionar.value)},00 adicionado com sucesso!`)
-        escreverInnerHTML(imagens[0])
-    }
-}
-function Quebrar() {
-    let total = armazenado.reduce((soma, i) => { return soma + i })
-    if (total < 1000) {
+const Adicionar = document.querySelector("#btnAdicionar")
+Adicionar.addEventListener('click', evento => {
+    const inputAdicionar = document.querySelector("#inputAdicionar").value
+    console.log(inputAdicionar);
+    inputAdicionar < 1 ? (alert('Adicione um valor maior!')) :
+        (armazenado.push(inputAdicionar))
+        (saldo.innerHTML = "Uhul, o dinheiro foi adicionado!")
+        (alert(`R$ ${(inputAdicionar)},00 adicionado com sucesso!`))
+        (escreverInnerHTML(imagens[0]))
+});
+const Quebrar = document.querySelector('#Quebrar')
+Quebrar.addEventListener('click', evento => {
+    let total = armazenado.reduce((soma, i) =>  soma + i )
+    if (total < 1 || armazenado == [])  {
+        alert("Não há dinheiro no porquinho!")
+    }else if (total < 1000) {
         saldo.innerHTML = `Seu porquinho não é gordo o suficiente!`
         escreverInnerHTML(imagens[2])
     } else {
         saldo.innerHTML = `${(total).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`
         escreverInnerHTML(imagens[1])
     }
-}
+
+}) 
+
 function escreverInnerHTML(texto){
      return porquinho.innerHTML = `<img width="300" src="./imagens/${texto}">`
 }
