5e6a701d2ecfc931eadedb555a6ce20b83eebd29
IagoBonfim <iagoBonfim@users.noreply.github.com>
2023-10-13 22:42:24 -0300

    Update script.js
    armazenamento no Local Storage dos valores recebidos nos botões / organização na estrutura do código.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const saldo = document.querySelector('#saldo')
 let porquinho = document.querySelector('.imagemPorquinho')
 const imagens = ['Adicionar_porquinho.jpg', 'Quebrado.png', 'magro.jpg']
 
-const Adicionar = document.querySelector("#btnAdicionar")
 let armazenado = new Array();
+const Adicionar = document.querySelector("#btnAdicionar")
 Adicionar.addEventListener('click', evento => {
     const inputAdicionar = document.querySelector("#inputAdicionar").value
 
@@ -15,39 +15,13 @@ Adicionar.addEventListener('click', evento => {
         if (localStorage.hasOwnProperty("armazenado")) {
             armazenado = JSON.parse(localStorage.getItem('armazenado'))
         }
-        armazenado.push(inputAdicionar)
+        armazenado.push(parseInt(inputAdicionar))
         localStorage.setItem('armazenado', JSON.stringify(armazenado))
         saldo.innerHTML = "Uhul, o dinheiro foi adicionado!"
         alert(`R$ ${(inputAdicionar)},00 adicionado com sucesso!`)
         escreverInnerHTML(imagens[0])
     }
 });
-const Quebrar = document.querySelector('#Quebrar')
-Quebrar.addEventListener('click', evento => {
-    let valoresResgatados = localStorage.getItem("armazenado", JSON.stringify('armazenado'))
-    let total = []
-    JSON.parse(valoresResgatados).forEach(item => {
-        total.push(parseInt(item))
-        console.log(total)
-    });
-
-    let acumulado = total.reduce((valor, soma) => valor + soma)
-
-    if (acumulado < 1 || total == []) {
-        alert("Não há dinheiro no porquinho!")
-    } else if (acumulado < 1000) {
-        saldo.innerHTML = `Seu porquinho não é gordo o suficiente!`
-        escreverInnerHTML(imagens[2])
-    } else {
-        saldo.innerHTML = `${(acumulado).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`
-        escreverInnerHTML(imagens[1])
-    }
-
-})
-
-function escreverInnerHTML(texto) {
-    return porquinho.innerHTML = `<img width="300" src="./imagens/${texto}">`
-}
 const adicionarValorDoBotao = document.querySelectorAll('button')
 adicionarValorDoBotao.forEach((elemento) => {
     elemento.addEventListener('click', evento => {
@@ -57,8 +31,29 @@ adicionarValorDoBotao.forEach((elemento) => {
             if (localStorage.hasOwnProperty("armazenado")) {
                 armazenado = JSON.parse(localStorage.getItem('armazenado'))
             }
-            armazenado.push(valorDoBotao)
+            armazenado.push(parseInt(valorDoBotao))
+            localStorage.setItem('armazenado', JSON.stringify(armazenado))
+            console.log(armazenado)
             alert(`R$ ${valorDoBotao},00 adicionado com sucesso!`)
         }
     })
 })
+const Quebrar = document.querySelector('#Quebrar')
+Quebrar.addEventListener('click', evento => {
+    let valoresResgatados =  JSON.parse(localStorage.getItem("armazenado"))
+    let total = valoresResgatados.reduce((valor, soma) => valor + soma)
+    
+    if (total < 1 || valoresResgatados == []) {
+        alert("Não há dinheiro no porquinho!")
+    } else if (total < 1000) {
+        saldo.innerHTML = `Seu porquinho não é gordo o suficiente!`
+        escreverInnerHTML(imagens[2])
+    } else {
+        saldo.innerHTML = `${(total).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`
+        escreverInnerHTML(imagens[1])
+    }
+})
+
+function escreverInnerHTML(texto) {
+    return porquinho.innerHTML = `<img width="300" src="./imagens/${texto}">`
+}
